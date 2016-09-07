bb8acedc3329625120a9ad016db097a3cd18a832
Alexandru Duta <aduta@endava.net>
2016-09-07 16:40:48 +0300

    first

---
+++ CSharpAutoTraining/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Curs1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // simple Hello World program in C#
+            Console.WriteLine("Hello world");
+            Console.ReadKey();
+        }
+    }
+}
