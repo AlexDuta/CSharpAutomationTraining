ee5f1159ef8a2811aaabac90b5b2d751cabb4a17
Larisa Popa <larisa.popa@endava.com>
2021-03-30 11:02:04 +0300

    practice Session1

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Browser
+    {
+        //variable
+        public int WindowWidth = 1024;
+
+
+
+        //public Browser()
+        //{
+
+        //}
+
+
+        public Browser(int newWidowWidth)
+        {
+            WindowWidth = newWidowWidth;
+        }
+
+        //method
+        public void ChangeWindowWidth(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+    }
+}
--- CSharpAutoTraining/Program.cs
+++ CSharpAutoTraining/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpAutoTraining.Curs1;
+using System;
 
 namespace Curs1
 {
@@ -7,6 +8,14 @@ namespace Curs1
         static void Main(string[] args)
         {
             // simple Hello World program in C#
+            var browser = new Browser(780);
+
+
+            Console.WriteLine(browser.WindowWidth);
+            // browser.ChangeWindowWidth(100);
+            //Console.WriteLine(browser.WindowWidth);
+
+
             Console.WriteLine("Hello world");
             Console.ReadKey();
         }
