0451942cbf0e9ef97af7da4e403b8484e76bf3e2
raulbobletec95 <raul.bobletec@endava.com>
2021-03-30 16:20:05 +0300

    Tema 1

--- CSharpAutoTraining/Curs1/Browser.cs
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -9,17 +9,24 @@ namespace CSharpAutoTraining.Curs1
     class Browser
     {
         //variable
-        public int WindowWidth = 1024;
-
-        public Browser(int newWindowWitdh)
-        {
-            WindowWidth = newWindowWitdh;
-        }
+        public int Height = 0;
+        public int Width = 0;
 
         //method
+        public void changeWindowHeight(int newWindowHeight)
+        {
+            Height = newWindowHeight;
+        }
+
         public void changeWindowWidth(int newWindowWidth)
         {
-            WindowWidth = newWindowWidth;
+            Width = newWindowWidth;
+        }
+
+        public void showWindowHeightandWidth()
+        {
+            Console.WriteLine("The browser's new resolution is: " + Height + " x " + Width);
+            Console.ReadKey();
         }
     }
 }
---
+++ CSharpAutoTraining/Curs1/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            Browser browser = new Browser();
+            browser.changeWindowHeight(1080);
+            browser.changeWindowWidth(1920);
+            browser.showWindowHeightandWidth();
+            
+            
+        }
+    }
+}
