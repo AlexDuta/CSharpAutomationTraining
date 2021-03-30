0b0606929226112d844b2436df80376f6dd90b83
Andrei Teodor Pop [Intern] <AndreiTeodor.Pop@endava.com>
2021-03-30 11:01:55 +0300

    practice Session1

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Browser
+    {
+        //variable
+        public int WindowWidth = 1024;
+
+        public Browser()
+        {
+
+        }
+
+        public Browser(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+
+        //method
+        public void ChangeWindowWidth(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+    }
+}
