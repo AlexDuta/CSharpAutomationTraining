e6fe3248b3149424ae40483e020a3ba2d2539a46
raulbobletec95 <raul.bobletec@endava.com>
2021-03-30 11:01:47 +0300

    First commit

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Browser
+    {
+        //variable
+        public int WindowWidth = 1024;
+
+        public Browser(int newWindowWitdh)
+        {
+            WindowWidth = newWindowWitdh;
+        }
+
+        //method
+        public void changeWindowWidth(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+    }
+}
--- CSharpAutoTraining/Program.cs
+++ CSharpAutoTraining/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharpAutoTraining.Curs1;
+using System;
 
 namespace Curs1
 {
@@ -6,9 +7,15 @@ namespace Curs1
     {
         static void Main(string[] args)
         {
+            Browser br = new Browser(500);
+            Console.WriteLine(br.WindowWidth);
+            //br.changeWindowWidth(100);
+            //Console.WriteLine(br.WindowWidth);
+
             // simple Hello World program in C#
             Console.WriteLine("Hello world");
             Console.ReadKey();
+
         }
     }
 }
