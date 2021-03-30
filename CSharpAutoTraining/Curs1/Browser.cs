cecbd9c83fad1f52c09b39fc716e31ae99382416
cristian.negrea <cristian.negrea@endava.com>
2021-03-30 17:05:43 +0300

    tema 1

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Browser
+    {
+        //variable
+        public int WindowHeight;
+        public int WindowWidth;
+
+        public Browser()
+        {
+
+        }
+
+        public Browser(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+
+        //method
+        public void ChangeWindowHeight(int newWindowHwight)
+        {
+            WindowHeight = newWindowHwight;
+        }
+
+        public void ChangeWindowWidth(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+
+        public void DisplayNewWindowHeightAndWidth()
+        {
+            Console.WriteLine("New Browser resolution is: ");
+            Console.WriteLine("New WindowHight is: " + WindowHeight);
+            Console.WriteLine("New WindowWidth is: " + WindowWidth);
+        }
+    }
+}
