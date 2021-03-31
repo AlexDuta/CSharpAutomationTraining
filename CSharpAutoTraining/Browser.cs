b60fb29b87f69cfc9b0701e8d3507f292e0de908
ENDAVA\ebizau <m_elena26@yahoo.com>
2021-03-31 23:26:05 +0300

    Browser problem

---
+++ CSharpAutoTraining/Browser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining
+{
+
+    class Browser
+    {
+       private double Height = 0;
+       private double Width = 0;
+
+        public void DisplayHeightAndWidth()
+        {
+            Console.WriteLine("Height " + Height + ", width "+ Width);
+
+        }
+        public void ChangeWindowHeight(double Height)
+        {
+            this.Height = Height;
+
+        }
+        public void ChangeWindowWidth(double Width)
+        {
+            this.Width = Width;
+        }
+
+
+    }
+}
---
+++ CSharpAutoTraining/Test.cs
@@ -0,0 +1,20 @@
+﻿using CSharpAutoTraining;
+using System;
+
+namespace Curs1
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Browser browser = new Browser();
+            browser.ChangeWindowHeight(1080);
+            browser.ChangeWindowWidth(1920);
+
+            browser.DisplayHeightAndWidth();
+            
+
+         
+        }
+    }
+} 
