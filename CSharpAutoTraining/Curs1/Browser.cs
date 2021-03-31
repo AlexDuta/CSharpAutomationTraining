7037991d5afe2fa82423163ec9edeebe9f345768
diana <diana.bardasan@endava.com>
2021-03-31 10:49:25 +0300

    Homework1

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Browser
+    {
+        public Browser()
+        {
+
+        }
+
+        public Browser(int newWindowHeight)
+        {
+            WindowHeight = newWindowHeight;
+        }
+
+       // Browser(int newWindowWidth)
+       // {
+       //     WindowWidth = newWindowWidth;
+       // }
+
+        //variable
+        public int WindowHeight;
+        public int WindowWidth;
+
+
+        //method
+        public void ChangeWindowHeight(int newWindowHeight)
+        {
+            WindowHeight = newWindowHeight;
+        }
+
+        public void ChangeWindowWidth(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+
+        public void DisplayNewWindowHeightAndWidth()
+        {
+            Console.WriteLine("New Browser resolution is: ");
+            Console.WriteLine("New Window height is: " + WindowHeight);
+            Console.WriteLine("New Window width is: " + WindowWidth);
+        }
+    }
+}
---
+++ CSharpAutoTraining/Curs1/Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Test
+    {
+        public static void Main(String[] args) {
+            Browser browser = new Browser();
+            browser.ChangeWindowHeight(1080);
+            browser.ChangeWindowWidth(1920);
+            browser.DisplayNewWindowHeightAndWidth();
+            Console.ReadKey();
+        }
+    }
+}
