8d4963070eff29dbadb9048297cf66b7cd088a21
anda <andrada.iordache@endava.com>
2021-04-01 17:38:44 +0300

    Am adaugat tema1

---
+++ CSharpAutoTraining/Curs1/Browser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Browser
+    {
+        public int Height;
+        public int Width;
+        
+        public void ChangeWindowHeight(int newHeight)
+        {
+            Height = newHeight;
+        }
+        public void ChangeWindowWidth(int newWidth)
+        {
+            Width = newWidth;
+        }
+        public void DisplayWindowHeight()
+        {
+            Console.WriteLine($"Window Height is {Height}");
+        }
+        public void DisplayWindowWidth()
+        {
+            Console.WriteLine($"Window Width is {Width}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpAutoTraining/Curs1/Test.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CSharpAutoTraining.Curs1;
+
+namespace CSharpAutoTraining.Curs1
+{
+    class Test
+    {
+        public static void Main(String[] args)
+        {
+            Browser browser = new Browser();
+            browser.ChangeWindowHeight(1080);
+            browser.ChangeWindowWidth(1920);
+            browser.DisplayWindowHeight();
+            browser.DisplayWindowWidth();
+        }
+    }
+}
