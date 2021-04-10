90433841cc98e690d77008d1f53cd89efcdac117
ENDAVA\ebizau <m_elena26@yahoo.com>
2021-04-10 18:03:43 +0300

    Curs 7

---
+++ TestClassLibrary/Curs7/Browser.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+[TestFixture]
+
+    public class Browser
+    {
+    public static bool StartBrowser(bool status)
+    {
+        return status;
+    }
+    public static string GetTitle(string OpenedBrowser)
+    {
+        return OpenedBrowser;
+    }
+    public static int GetBrowserWidth(int BrowserWidth)
+    {
+        return BrowserWidth;
+    }
+    }
+
---
+++ TestClassLibrary/Curs7/Test.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+[TestFixture]
+
+class Test
+{
+    bool TestBool = Browser.StartBrowser(false);
+    bool TestBool2 = Browser.StartBrowser(true);
+    string TestString = Browser.GetTitle("Browser is open");
+    int TestInt = Browser.GetBrowserWidth(2);
+
+   
+    }
+
