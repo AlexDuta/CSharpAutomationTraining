ad7bd98f10af060618ffe07d93255712f3873482
raulbobletec95 <raul.bobletec@endava.com>
2021-04-12 11:56:53 +0300

    Tema Curs 7

---
+++ TestClassLibrary/Curs7/Browser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibrary.Curs7
+{
+    class Browser
+    {
+        int BrowserWidth = 50;
+        public bool StartBrowser()
+        {
+            return true;
+        }
+        
+        public string GetPageTitle()
+        {
+            return "Browser is open!";
+        }
+
+        public int GetBrowserWidth()
+        {
+            return BrowserWidth;
+        }
+
+
+
+    }
+}
---
+++ TestClassLibrary/Test.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestClassLibrary.Curs7;
+
+namespace TestClassLibrary
+{
+    [TestFixture]
+    public class Test
+    {
+        Browser br = new Browser();
+        [Test]
+        public void TestBrowserTrue()
+        {
+            Assert.IsTrue(br.StartBrowser());
+        }
+        
+        [Test]
+        public void TestBrowserFalse()
+        {
+            Assert.IsFalse(br.StartBrowser());
+        }
+        [Test]
+        public void GetPageTitleEqual()
+        {
+            Assert.AreEqual("Browser is open!", br.GetPageTitle());
+        }
+        
+        //[Test]
+        //public void GetPageTitleContains()
+        //{
+        //    Assert.Contains("open", br.GetPageTitle());
+        //}
+
+        [Test]
+        public void GetBrowserWidthEqual()
+        {
+            Assert.AreEqual(50, br.GetBrowserWidth());
+        }
+
+        [Test]
+        public void GetBrowserWidthLess()
+        {
+            Assert.Less(40, br.GetBrowserWidth());
+        }
+
+        [Test]
+        public void GetBrowserWidthGreater()
+        {
+            Assert.Greater(60, br.GetBrowserWidth());
+        }
+    }
+}
