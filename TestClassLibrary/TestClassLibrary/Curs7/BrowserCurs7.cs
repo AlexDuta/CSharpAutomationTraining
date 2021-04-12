3d0c52580e4fb53dee314431efb135361d989179
diana <diana.bardasan@endava.com>
2021-04-12 20:49:58 +0300

    Homework Curs 7

---
+++ TestClassLibrary/TestClassLibrary/Curs7/BrowserCurs7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibrary.Curs7
+{
+    class BrowserCurs7
+    {
+        int BrowserWidth = 10;
+        public bool StartBrowser()
+        {
+            return true;
+        }
+
+        public string GetPageTitle()
+        {
+            return "Browser is open!";
+        }
+
+        public int GetBrowserWidth()
+        {
+            return BrowserWidth;
+        }
+    }
+}
---
+++ TestClassLibrary/TestClassLibrary/Curs7/Test.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TestClassLibrary.Curs7
+{
+    [TestFixture]
+    class Test
+    {
+        BrowserCurs7 browser = new BrowserCurs7();
+       [Test]
+        public void StartBrowserTrue()
+        {
+            Assert.IsTrue(browser.StartBrowser());
+        }
+
+        [Test]
+        public void StartBrowserFalse()
+        {
+            Assert.IsFalse(browser.StartBrowser());
+        }
+
+        [Test]
+        public void GetPageTitleEquals()
+        {
+            Assert.AreEqual("Browser is open!", browser.GetPageTitle());
+        }
+
+        //[Test]
+        //public void GetPageTitleContains()
+        //{
+        //    Assert.Contains("Browser is open!", browser.GetPageTitle());
+        //}
+
+        [Test]
+        public void GetPageTitleContains()
+        {
+            String valoare = browser.GetPageTitle();
+            Assert.IsTrue(valoare.Contains("Browser is open!"));
+        }
+        [Test]  
+        public void GetBrowserWidthEqual()
+        {
+            Assert.AreEqual(10, browser.GetBrowserWidth());
+        }
+
+        [Test]
+        public void GetBrowserWidthLess()
+        {
+            Assert.Less(8, browser.GetBrowserWidth());
+        }
+
+        [Test]
+        public void GetBrowserWidthGreater()
+        {
+            Assert.Greater(12, browser.GetBrowserWidth());
+        }
+
+    }
+}
