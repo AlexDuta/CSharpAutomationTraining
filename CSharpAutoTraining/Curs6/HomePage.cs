2b0d62f312d2925ee1d02f840e26baecca7a3200
cristian.negrea <cristian.negrea@endava.com>
2021-04-12 12:56:39 +0300

    modificari tema Curs6

--- CSharpAutoTraining/Curs6/HomePage.cs
+++ CSharpAutoTraining/Curs6/HomePage.cs
@@ -8,13 +8,13 @@ namespace CSharpAutoTraining.Curs6
 {
     class HomePage
     {
-        String Image = "//*[@id="header"]/a/img";
-        String Homelink = "//*[@id="navHeader"]/a[1]";
+        String Image = "//*[@id='header']/a/img";
+        String Homelink = "//*[@id='navHeader']/a[1]";
         String HTML = "/html/body/h1";
         String textFromPage = "/html/body/p";
-        String Email = "//*[@id="email"]";
-        String passwordText = "//*[@id="myDivHeader"]/form/text()[2]";
-        String Login = "//*[@id="Login"]";
-        String Home = "//*[@id="nav"]/li[1]/a";
+        String Email = "//*[@id='email']";
+        String passwordText = "//*[@id=/'myDivHeader']/form/text()[2]";
+        String Login = "//*[@id='Login']";
+        String Home = "//*[@id=nav']/li[1]/a";
     }
 }
