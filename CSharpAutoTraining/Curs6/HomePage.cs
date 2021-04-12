52e9271ba11ea6859b12a04549e71bae0b489e4e
diana <diana.bardasan@endava.com>
2021-04-12 19:18:10 +0300

    homeworkCurs6

--- CSharpAutoTraining/Curs6/HomePage.cs
+++ CSharpAutoTraining/Curs6/HomePage.cs
@@ -8,13 +8,13 @@ namespace CSharpAutoTraining.Curs6
 {
     class HomePage
     {
-        String image = "//*[@id="header"]/a/img";
-        String homeLink = "//*[@id="navHeader"]/a[1]";
+        String image = "//*[@id='header']/a/img";
+        String homeLink = "//*[@id='navHeader']/a[1]";
         String html = "/html/body/h1";
         String textFromPage = "/html/body/p";
-        String email = "//*[@id="email"]";
-        String password = "//*[@id="myDivHeader"]/form/text()[2]";
-        String loginButton = "//*[@id="Login"]";
-        String home = "//*[@id="nav"]/li[1]/a";
+        String email = "//*[@id='email']";
+        String password = "//*[@id='myDivHeader']/form/text()[2]";
+        String loginButton = "//*[@id='Login']";
+        String home = "//*[@id='nav']/li[1]/a";
     }
 }
