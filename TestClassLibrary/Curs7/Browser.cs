282c6e4e0332395eda82a5563b0598942d2d6fd1
Andrei Teodor Pop [Intern] <AndreiTeodor.Pop@endava.com>
2021-04-12 17:31:38 +0300

    Curs 7 homework

---
+++ TestClassLibrary/Curs7/Browser.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestClassLibrary.Curs7
+{
+    class Browser
+    {
+        private int BrowserWidth;
+
+        public Browser()
+        {
+            this.BrowserWidth = 1920;
+        }
+        public Boolean StartBrowser()
+        {
+            return true;
+        }
+
+        public string GetPageTitle()
+        {
+            string pageTitle = "Browser is open.";
+            return pageTitle;
+        }
+
+        public int GetBrowserWidth()
+        {
+            return BrowserWidth;
+        }
+    }
+}
