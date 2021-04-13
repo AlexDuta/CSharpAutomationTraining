f0ee6f356b43c7ad15ef7582038694cbdd4a0473
popandrei97 <andrei_teodor.pop@yahoo.com>
2021-04-13 18:17:09 +0300

    Curs 9 homework

--- TestClassLibrary/Curs9/TestCurs9.cs
+++ TestClassLibrary/Curs9/TestCurs9.cs
@@ -1,76 +1,72 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace TestClassLibrary.Curs8
+namespace TestClassLibrary.Curs9
 {
     [TestFixture]
-    class TestCurs8
+    class TestCurs9
     { 
-
         public IWebDriver driver;
 
-        [Test]
-        public void CheckPageTitleTest()
+        [SetUp]
+        public void SetUp()
         {
             driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
             driver.Url = @"C:\Users\atpop\OneDrive - ENDAVA\Desktop\homepage.html";
             driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [Test]
+        public void CheckPageTitleTest()
+        {
             Assert.IsTrue(driver.Title.Equals("Home page"));
-            driver.Close();
         }
 
         [Test]
         public void EmailInputTest()
         {
-            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
-            driver.Url = @"C:\Users\atpop\OneDrive - ENDAVA\Desktop\homepage.html";
-            driver.Manage().Window.Maximize();
             var emailInput = driver.FindElement(By.Id("email"));
             Assert.IsTrue(emailInput.Displayed);
-            driver.Close();
         }
 
         [Test]
         public void PasswordInputTest()
         {
-            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
-            driver.Url = @"C:\Users\atpop\OneDrive - ENDAVA\Desktop\homepage.html";
-            driver.Manage().Window.Maximize();
             var passwordInput = driver.FindElement(By.Id("password"));
             Assert.IsTrue(passwordInput.Displayed);
-            driver.Close();
         }
 
         [Test]
         public void LoginButtonTest()
         {
-            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
-            driver.Url = @"C:\Users\atpop\OneDrive - ENDAVA\Desktop\homepage.html";
-            driver.Manage().Window.Maximize();
             var loginButton = driver.FindElement(By.Id("Login"));
             Assert.IsTrue(loginButton.Displayed);
-            driver.Close();
         }
 
         [Test]
         public void LinksDisplayedTest()
         {
-            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
-            driver.Url = @"C:\Users\atpop\OneDrive - ENDAVA\Desktop\homepage.html";
-            driver.Manage().Window.Maximize();
             var header = driver.FindElement(By.Id("navHeader"));
             var linksHeader = header.FindElements(By.XPath(".//*[contains(@href, 'html')]"));
             var footer = driver.FindElement(By.Id("nav"));
             var linksFooter = footer.FindElements(By.XPath(".//*[contains(@href, 'html') or contains(@href,'#')]"));
-            foreach(var element in linksHeader)
+            foreach (var element in linksHeader)
             {
                 Assert.IsTrue(element.Displayed);
                 Console.WriteLine(element.Text + element.GetAttribute("html"));
@@ -80,7 +76,30 @@ namespace TestClassLibrary.Curs8
                 Assert.IsTrue(element.Displayed);
                 Console.WriteLine(element.Text + element.GetAttribute("html"));
             }
-            driver.Close();
         }
+
+        [Test]
+        public void ErrorMessageDisplayedTest()
+        {
+            var emailInput = driver.FindElement(By.Id("email"));
+            //emailInput.SendKeys("");
+            var emailErrorText = driver.FindElement(By.Id("emailErrorText"));
+
+            var passwordInput = driver.FindElement(By.Id("password"));
+            //passwordInput.SendKeys("");
+            var passwordErrorText = driver.FindElement(By.Id("passwordErrorText"));
+
+            var loginButton = driver.FindElement(By.XPath("//*[@id='Login']"));
+            loginButton.Click();
+
+            Assert.IsTrue(emailErrorText.Displayed);
+            Assert.IsTrue(passwordErrorText.Displayed);
+
+            // Actions actions = new Actions(driver);
+            // actions.SendKeys(Keys.F5).Perform();
+            driver.Navigate().Refresh();
+
+            Thread.Sleep(3000);
+        }        
     }
 }
