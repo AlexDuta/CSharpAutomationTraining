d77dbd0a52477dc1be983262d21831d6d98634cd
raulbobletec95 <raul.bobletec@endava.com>
2021-04-14 12:09:22 +0300

    Tema 8 si 9

--- TestClassLibrary/Test.cs
+++ TestClassLibrary/Test.cs
@@ -1,7 +1,12 @@
 ﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using TestClassLibrary.Curs7;
@@ -11,6 +16,22 @@ namespace TestClassLibrary
     [TestFixture]
     public class Test
     {
+        public IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Driver");
+            driver.Url = @"C:\Users\rbobletec\Downloads\pages\homepage.html";
+            driver.Manage().Window.Maximize();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
         Browser br = new Browser();
         [Test]
         public void TestBrowserTrue()
@@ -58,6 +79,83 @@ namespace TestClassLibrary
         public void GetBrowserWidthGreater()
         {
             Assert.Greater(60, br.GetBrowserWidth());
+        }
+
+                            //Curs 8
+
+
+        [Test]
+        public void checkPageTitle()
+        {
+            Assert.IsTrue(driver.Title.Equals("Home page"));
+        }
+
+        [Test]
+        public void checkEmailInputDisplayed()
+        {
+            Assert.IsTrue(driver.FindElement(By.Id("email")).Displayed);
+        }
+
+        [Test]
+        public void checkPasswordInputDisplayed()
+        {
+            Assert.IsTrue(driver.FindElement(By.Id("password")).Displayed);
+        }
+
+        [Test]
+        public void loginButtonDisplayed()
+        {
+            Assert.IsTrue(driver.FindElement(By.Id("Login")).Displayed);
+        }
+
+        [Test]
+        public void findLinks()
+        {
+            var elements = driver.FindElements(By.TagName("a"));
+            
+            foreach(var e in elements)
+            {
+                Assert.IsTrue(e.Displayed);
+            }
+        }
+
+
+                                //Curs9
+        
+        [Test]
+        public void checkEmailAndPasswordErrorMessage()
+        {
+
+            driver.FindElement(By.Id("Login")).Click();
+            Assert.IsTrue(driver.FindElement(By.Id("emailErrorText")).Displayed);
+            Assert.IsTrue(driver.FindElement(By.Id("passwordErrorText")).Displayed);
+
+            driver.Navigate().Refresh();
+
+            Assert.IsFalse(driver.FindElement(By.Id("emailErrorText")).Displayed);
+            Assert.IsFalse(driver.FindElement(By.Id("passwordErrorText")).Displayed);
+        }
+
+        [Test]
+        public void checkHeaderLinks()
+        {
+            var element = driver.FindElement(By.Id("navHeader"));
+            var links = element.FindElements(By.XPath(".//a[contains(@href, 'long')]"));
+            foreach (var e in links)
+            {
+                Assert.IsTrue(e.Displayed);
+            }
+        }
+
+        [Test]
+        public void checkFooterLinks()
+        {
+            var element = driver.FindElement(By.Id("nav"));
+            var links = element.FindElements(By.XPath(".//a[contains(@href, 'long')]"));
+            foreach (var e in links)
+            {
+                Assert.IsTrue(e.Displayed);
+            }
         }
     }
 }
