b2648c959c8826faf234ad946d73835a3c8f6467
Rebeca Szucs <szucsrebeca@gmail.com>
2022-02-15 10:56:08 +0200

    Curs 1 Presentation

---
+++ CSharpAutoTraining/Course1/Browser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Course1
+{
+    class Browser
+    {
+        // variable
+        public int WindowWidth = 1024;
+        //method
+        Browser() { }
+        public Browser(int newWindowWidth)
+        {
+            WindowWidth = newWindowWidth;
+        }
+        public void ChangeWindowWidth( int newWindowWidth)
+        {
+
+        }
+    }
+}
--- CSharpAutoTraining/Program.cs
+++ CSharpAutoTraining/Program.cs
@@ -8,7 +8,15 @@ namespace Curs1
         {
             // simple Hello World program in C#
             Console.WriteLine("Hello world");
+           
+            Browser browser = new Browser(4000);
+
+            Console.WriteLine(browser.WindowWidth);
+            browser.ChangeWindowWidth(3000);
+            Console.WriteLine(browser.WindowWidth);
             Console.ReadKey();
+
+
         }
     }
 }
