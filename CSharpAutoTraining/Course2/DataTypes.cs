80cbe37e17bdb8bd4e0b78d3cf96bd603c57e49a
Andreea Ioana Dragu <Andreea-Ioana.Dragu@endava.com>
2022-02-18 00:48:49 +0200

    Message

---
+++ CSharpAutoTraining/Course2/DataTypes.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Course2
+{
+    public class DataTypes
+    {
+
+        // Variables and their data types
+        byte a1 = 255;
+        sbyte a2 = -128;
+        short a3 = -32765;
+        ushort a4 = 65530;
+        int a5 = 100;
+        uint a6 = 0;
+        long a7 = -98765432345678;
+        ulong a8 = 12345678765432345678;
+        float a9 = 0.1234567f;
+        double a10 = 2.34567654d;
+        decimal a11 = -76545678876533535M;
+        char a12 = 'a';
+        string a13 = "Course2";
+        bool a14 = true;
+        object a15 = 10;    // Automatically cast to int
+
+        // Display all variables
+        public void display()
+        {
+            Console.WriteLine("byte: " + a1 + "\n" + 
+                              "sbyte: " + a2 + "\n" + 
+                              "short: " + a3 + "\n" + 
+                              "ushort: " + a4 + "\n" + 
+                              "int: " + a5 + "\n" +
+                              "uint: " + a6 + "\n" + 
+                              "long: " + a7 + "\n" + 
+                              "ulong: " + a8 + "\n" + 
+                              "float: " + a9 + "\n" + 
+                              "double: " + a10 + "\n" +
+                              "decimal: " + a11 + "\n" + 
+                              "char: " + a12 + "\n" + 
+                              "string: " + a13 + "\n" + 
+                              "bool: " + a14 + "\n" + 
+                              "object: " + a15 + "\n");
+        }
+
+        // Pre Increment function
+        public void PreIncrement(int x)
+        {
+            Console.WriteLine(x++);
+        }
+
+        // Post Increment function
+        public void PostIncrement(int x)
+        {
+            Console.WriteLine(++x);
+        }
+
+        // Relational Operators functions
+        public void PlusEq(int x, int y)
+        {
+            x += y;
+            Console.WriteLine("+= " + x);
+        }
+        public void MinusEq(int x, int y)
+        {
+            x -= y;
+            Console.WriteLine("-= " + x);
+        }
+        public void MulEq(int x, int y)
+        {
+            x *= y;
+            Console.WriteLine("*= " + x);
+        }
+        public void DivEq(float x, float y)
+        {
+            x /= y;
+            Console.WriteLine("/= " + x);
+        }
+
+        // If
+        public void IfStatement(int x, int y)
+        {
+            int value = x / y;
+            if(value == 4)
+            {
+                Console.WriteLine(true);
+            }
+        }
+
+        // If-Else
+        public void IfElseStatement(int x, int y)
+        {
+            int value = x / y;
+            if(value == 1)
+            {
+                Console.WriteLine("One");
+            }
+            else if(value > 0)
+            {
+                Console.WriteLine("Positive");
+            }
+            else 
+            {
+                Console.WriteLine("Negative");
+            }
+        }
+
+        // Switch-Case
+        public void SwitchCase(int x)
+        {
+            switch (x)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("One");
+                        break;
+                    }
+                    
+                case 2:
+                    {
+                        Console.WriteLine("Two");
+                        break;
+                    }
+                case 3:
+                    {
+                        Console.WriteLine("Three");
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Bigger");
+                        break;
+                    }
+                
+            }
+        }
+
+        // While
+        public void WhileLoop(int i)
+        {
+            i = 0;
+            while (i < 10)
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+            Console.WriteLine();
+        }
+
+        // For 
+        public void ForLoop(int x)
+        {
+            for(int i=0; i<x; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        // Do-While 
+        public void DoWhileLoop(int x)
+        {
+            int i = 0;
+            do
+            {
+                Console.Write(i + " ");
+                i++;
+            }
+            while (i < x);
+            Console.WriteLine();
+        }
+    }
+}
--- CSharpAutoTraining/Program.cs
+++ CSharpAutoTraining/Program.cs
@@ -1,4 +1,5 @@
 ﻿using CSharpAutoTraining.Course1;
+using CSharpAutoTraining.Course2;
 using System;
 
 namespace Course1
@@ -7,7 +8,7 @@ namespace Course1
     {
         static void Main(string[] args)
         {
-            /* COURSE 1 */
+            /* COURSE 1 
 
             // Hello World program
             Console.WriteLine("Hello World!");
@@ -24,6 +25,66 @@ namespace Course1
             browser.DisplayWindowWidthHeight();
             Console.WriteLine();
 
+            */
+
+
+            /* COURSE 2 */
+
+            // Data Types
+            Console.WriteLine("DATA TYPES");
+            DataTypes ds = new DataTypes();
+            ds.display();
+            Console.WriteLine();
+
+            // Conversions
+            Console.WriteLine("PRE & POST INCREMENT");
+            ds.PreIncrement(10);        // 10
+            ds.PostIncrement(10);       // 11
+            Console.WriteLine();
+
+            // Operators
+            Console.WriteLine("OPERATORS");
+            int x = 5;
+            int y = 3;
+            ds.PlusEq(x, y);
+            ds.MinusEq(x,y);
+            ds.MulEq(x,y);
+            ds.DivEq(x,y);
+            Console.WriteLine();
+
+            // If statement
+            Console.WriteLine("IF STATEMENT");
+            ds.IfStatement(8,2);
+            Console.WriteLine();
+
+            // If-Else statement
+            Console.WriteLine("IF-ELSE STATEMENT");
+            ds.IfElseStatement(4, 2);
+            ds.IfElseStatement(5, -3);
+            ds.IfElseStatement(7, 7);
+            Console.WriteLine();
+
+            // Switch-Case
+            Console.WriteLine("SWITCH-CASE");
+            ds.SwitchCase(1);
+            ds.SwitchCase(2);
+            ds.SwitchCase(8);
+            Console.WriteLine();
+
+            // While Loop
+            Console.WriteLine("WHILE LOOP");
+            ds.WhileLoop(10);
+            Console.WriteLine();
+
+            // For Loop
+            Console.WriteLine("FOR LOOP");
+            ds.ForLoop(10);
+            Console.WriteLine();
+
+            // Do-While Loop
+            Console.WriteLine("DO-WHILE LOOP");
+            ds.DoWhileLoop(10);
+            Console.WriteLine();
 
             Console.ReadLine();
             
