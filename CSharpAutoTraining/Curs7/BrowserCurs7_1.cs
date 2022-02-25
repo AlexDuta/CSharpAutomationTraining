f64a9246ef944b78b6b071a2d6407b4fd0bd5be0
Bogdan Bardi <bardibogdan@gmail.com>
2022-02-25 11:38:47 +0200

    Course7

---
+++ CSharpAutoTraining/Curs7/BrowserCurs7_1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Curs7
+{
+    class BrowserCurs7_1
+    {
+        bool started = false;
+        int WindowWidth = 1920;
+        public bool StartBrowser()
+        {
+            if (started)
+                return false;
+            started = true;
+            return true;
+        }
+        public string GetPageTitle()
+        {
+            if(started)
+                return "Browser is open";
+            return "No page";
+        }
+
+        public int GetBrowserWidth()
+        {
+            return WindowWidth;
+        }
+    }
+}
---
+++ TestClassLibrary/TestCurs7_1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CSharpAutoTraining.Curs7;
+
+namespace TestClassLibrary
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void TestStartBrowser()
+        {
+            BrowserCurs7_1 browser = new BrowserCurs7_1();
+            Assert.IsTrue(browser.StartBrowser());
+            Assert.IsFalse(browser.StartBrowser());
+        }
+        [Test]
+        public void TestGetPage()
+        {
+            BrowserCurs7_1 browser = new BrowserCurs7_1();
+            Assert.AreEqual("No page", browser.GetPageTitle());
+            Assert.IsTrue(browser.StartBrowser());
+            Assert.AreEqual("Browser is open", browser.GetPageTitle());
+
+        }
+        [Test]
+        public void TestGetBrowserWidth()
+        {
+            BrowserCurs7_1 browser = new BrowserCurs7_1();
+            Assert.Less(1000, browser.GetBrowserWidth());
+            Assert.AreEqual(1920, browser.GetBrowserWidth());
+            Assert.Greater(2000, browser.GetBrowserWidth());
+
+        }
+    }
+}
