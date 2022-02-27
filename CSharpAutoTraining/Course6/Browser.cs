59bf3d9e387d0a42f57e08ed776f293ecb090371
Andreea Ioana Dragu <Andreea-Ioana.Dragu@endava.com>
2022-02-27 21:29:17 +0200

    Course6

---
+++ CSharpAutoTraining/Course6/Browser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Course6
+{
+    class Browser
+    {
+
+        // Variable
+        public int WindowWidth = 1920;
+
+        // Method to return WindowWidth value
+        public int GetWindowWidth()
+        {
+            return WindowWidth;
+        }
+    }
+}
---
+++ CSharpAutoTraining/Course6/Start.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Course6
+{
+    class Start
+    {
+        // Method to return Browser object
+        public Browser CreateBrowser()
+        {
+            return new Browser();
+        }
+    }
+}
