bad839378de7b7f176650b17e7512b2056609d5e
Andreea Ioana Dragu <Andreea-Ioana.Dragu@endava.com>
2022-02-27 21:30:12 +0200

    Course6 Homework

---
+++ CSharpAutoTraining/Course6_HW/Firefox.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Course6_HW
+{
+    class Firefox
+    {
+        // Variable
+        public int WindowWidth;
+
+        // Default constructor
+        public Firefox()
+        {
+            Console.WriteLine("Default constructor.");
+            WindowWidth = 1080;
+        }
+
+        // Constructor with int parameter
+        public Firefox(int val)
+        {
+            Console.WriteLine("Constructor with int value.");
+            WindowWidth = val;
+        }
+
+        // Constructor with double parameter
+        public Firefox(double val)
+        {
+            Console.WriteLine("Constructor with double value.");
+            WindowWidth = (int)val;
+        }
+
+        // Getter for WindowWidth parameter
+        public int GetWindowWidth()
+        {
+            return WindowWidth;
+        }
+    }
+}
---
+++ CSharpAutoTraining/Course6_HW/HomePage.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAutoTraining.Course6_HW
+{
+    class HomePage
+    {
+        // String variables for specified XPaths
+        public string image = "//*[@id=\"header\"]/a/img";
+        public string homeLink = "//*[@id=\"myDivHeader\"]/header/ul/a[1]";
+        public string html = "//body/h1";
+        public string textFromPage = "//body/p/text()";
+        public string emailText = "//*[@id=\"myDivHeader\"]/form/text()[1]";
+        public string emailInput = "//body/div/form/input[@id=\"email\"]";
+        public string passText = "//*[@id=\"myDivHeader\"]/form/text()[2]";
+        public string passInput = "//body/div/form/input[@id=\"password\"]";
+        public string loginButton = "//*[@id=\"Login\"]";
+        public string home = "//footer/ul/li[1]/a";
+
+        // Method to display XPaths
+        public void DisplayXPaths()
+        {
+            Console.WriteLine("XPATHS: " +
+                              "\n\tImage: " + image +
+                              "\n\tHome Link: " + homeLink +
+                              "\n\tHTML: " + html +
+                              "\n\tText from Page: " + textFromPage +
+                              "\n\tE-mail Text: " + emailText +
+                              "\n\tE-mail Input: " + emailInput +
+                              "\n\tPassword Text: " + passText +
+                              "\n\tPassword Input: " + passInput +
+                              "\n\tLogin Button: " + loginButton +
+                              "\n\tHome Footer: " + home);
+        }
+         
+    }
+}
