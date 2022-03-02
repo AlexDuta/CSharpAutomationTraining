61c1d637f852cbf5bfec4d9840726ef6105770c8
Rebeca Szucs <szucsrebeca@gmail.com>
2022-03-02 14:38:55 +0200

    Tema7

---
+++ SeleniumProject/Class1.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SeleniumProject.Curs7;
+
+
+namespace SeleniumProject
+{
+
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void TestStartBrowser()
+        {
+            Browser browser= new Browser();
+            Assert.IsTrue(browser.StartBrowser());
+            Assert.AreEqual("False", browser.StartBrowser());
+        }
+
+        [Test]
+        public void TestGetPageTitle()
+        {
+            Browser browser = new Browser();
+            Assert.AreEqual("browser is open",browser.GetPageTitle());
+            //Assert.Contains("open",browser.GetPageTitle(), "contine stringul");
+            Assert.AreNotEqual("browser is closed", browser.GetPageTitle());
+        }
+
+        [Test]
+        public void TestGetBrowserWidth()
+        {
+            Browser browser = new Browser();
+            Assert.AreEqual(1890 , browser.GetBrowserWidth());
+            Assert.Less(1234, browser.GetBrowserWidth());
+            Assert.Greater(1999, browser.GetBrowserWidth());
+        }
+        [Test]
+        public void TestMethod1()
+        {
+            Console.WriteLine("test method1");
+            Assert.IsTrue(2 == 2);
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Console.WriteLine("test method2");
+            Assert.AreEqual(true, 2 == 3, "test2");
+        }
+    }
+}
---
+++ SeleniumProject/Curs7/Browser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace SeleniumProject.Curs7
+{
+    internal class Browser
+    {
+        public bool StartBrowser()
+        {
+            return true;
+        }
+        string title = "browser is open";
+        public string GetPageTitle()
+        {            
+            return title;
+        }
+        int width = 1890;
+        public int GetBrowserWidth()
+        {           
+            return width;
+        }
+    }
+}
