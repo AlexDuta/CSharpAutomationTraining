5db89d73418961adbd750dd29004af6f3e33fb7c
Denisa Paula Morar <Denisa-Paula.Morar@endava.com>
2022-03-04 01:14:06 +0200

    tema8

---
+++ SeleniumProject/TestClass.cs
@@ -0,0 +1,139 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium.Interactions;
+
+namespace SeleniumProject
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void FirstSeleniumTest()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///C:/Workspace/homepage.html";
+            Assert.True(driver.Title.Equals("Home page")); 
+            driver.FindElement(By.Id("email")).SendKeys("admin@domain.org"); 
+            driver.FindElement(By.Id("Login")).Click(); 
+            Assert.True(driver.FindElement(By.Id("email")).Displayed); 
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void PageTitleTest()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///C:/Workspace/homepage.html";
+            Assert.True(driver.Title.Equals("Home page"));
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void EmailInputTest()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///C:/Workspace/homepage.html";
+            Assert.True(driver.FindElement(By.Id("email")).Displayed);
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void PasswordInputTest()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///C:/Workspace/homepage.html";
+            Assert.True(driver.FindElement(By.Id("password")).Displayed);
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void LoginBtnTest()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///C:/Workspace/homepage.html";
+            Assert.True(driver.FindElement(By.Id("Login")).Displayed);
+            driver.Quit();
+
+        }
+
+        [Test]
+        public void LinksTest()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///C:/Workspace/homepage.html";
+            var elements = driver.FindElements(By.XPath("//a"));
+
+            foreach(var element in elements)
+            {
+                Assert.True(element.Displayed);
+            }
+            driver.Quit();
+
+        }
+
+        /*--tema 7
+        [Test]
+        public void TestMethod1()
+        {
+            Browser browser = new Browser();
+            Assert.IsTrue(browser.StartBrowser());
+         
+        }
+
+        [Test]
+        public void TestMethod2()
+        {
+            Browser browser = new Browser();
+            Assert.IsFalse(browser.StartBrowser());
+
+        }
+
+        [Test]
+        public void TestMethod3()
+        {
+            Browser browser = new Browser();
+            Assert.AreEqual("Browser is open",browser.GetPageTitle());
+
+        }
+
+        [Test]
+        public void TestMethod4()
+        {
+            Browser browser = new Browser();
+            Assert.AreEqual("Browser is not open", browser.GetPageTitle());
+
+        }
+
+        [Test]
+        public void TestMethod5()
+        {
+            Browser browser = new Browser();
+            Assert.AreEqual(1000, browser.GetBrowserWidth());
+
+        }
+
+        [Test]
+        public void TestMethod6()
+        {
+            Browser browser = new Browser();
+            Assert.Greater(1200, browser.GetBrowserWidth());
+
+        }
+
+        [Test]
+        public void TestMethod7()
+        {
+            Browser browser = new Browser();
+            Assert.Less(800, browser.GetBrowserWidth());
+
+        }*/
+    }
+}
