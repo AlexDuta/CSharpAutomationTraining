130981595445468b6ad6d8ac107f5d756335e15f
Denisa Paula Morar <Denisa-Paula.Morar@endava.com>
2022-03-04 00:22:10 +0200

    tema7

---
+++ SeleniumProject/Curs7/Browser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumProject.Curs7
+{
+    internal class Browser
+    {
+        int BrowserWidth = 1000;
+        public bool StartBrowser()
+        {
+            return true;
+        }
+
+        public string GetPageTitle()
+        {
+            return "Browser is open";
+        }
+
+        public int GetBrowserWidth()
+        {
+            return BrowserWidth;
+        }
+    }
+}
