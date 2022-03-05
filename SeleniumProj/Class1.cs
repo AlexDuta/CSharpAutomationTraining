33fd40e23f7b7db3fbbcc4b70449da28910e5982
leontiesdan <leontiesdan@yahoo.com>
2022-03-05 15:55:06 +0200

    Curs8HomeWork

--- SeleniumProj/Class1.cs
+++ SeleniumProj/Class1.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
-
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
 
 namespace SeleniumProj
 {
@@ -12,9 +15,37 @@ namespace SeleniumProj
     public class Class1
     {
         [Test]
-        public void FirstNUnitTest()
+        public void FirstSeleniumTest()
         {
-            Console.WriteLine("Test 1");
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)+ @"\Drivers");
+            driver.Url = "file:///c:/Workspace/homepage.html";
+            driver.Manage().Window.Maximize();
+            Assert.IsTrue(driver.Title.Equals("Home page"));
+
+            driver.FindElement(By.XPath("//input[@id='email']")).SendKeys("admin@domain.org");
+            driver.FindElement(By.Id("password")).SendKeys("111111");
+            //driver.FindElement(By.Id("email")).Clear();
+
+            driver.FindElement(By.Id("Login")).Click();
+
+            Assert.IsTrue(driver.Title.Equals("Dashboard page"));
+
+
+            //var passwordErrorText = driver.FindElement(By.Id("passwordErrorText"));
+
+            //Assert.IsTrue(passwordErrorText.Displayed);
+
+            var elementsList = driver.FindElements(By.XPath("//a"));
+
+            foreach(var elm in elementsList)
+            {
+                Assert.IsTrue(elm.Displayed);
+            }
+
+
+            driver.Quit();
+            //driver.Close();
+
         }
     }
 }
---
+++ TestClassLibrary/Curs8/Test.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.IO;
+using System.Reflection;
+
+namespace TestClassLibrary.Curs8
+{
+    [TestFixture]
+    public class Test
+    {
+
+        [Test]
+        public void TestPageTitle()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///c:/Workspace/homepage.html";
+            driver.Manage().Window.Maximize();
+            Assert.IsTrue(driver.Title.Equals("Home page"));
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestEmailInputDisplayed()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///c:/Workspace/homepage.html";
+            driver.Manage().Window.Maximize();
+            var emailInput = driver.FindElement(By.XPath("//input[@id='email']"));
+            Assert.IsTrue(emailInput.Displayed);
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestPasswordInputDisplayed()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///c:/Workspace/homepage.html";
+            driver.Manage().Window.Maximize();
+            var passwordInput = driver.FindElement(By.Id("password"));
+            Assert.IsTrue(passwordInput.Displayed);
+            driver.Quit();
+        }
+
+        [Test]
+        public void TestLoginBtnDisplayed()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///c:/Workspace/homepage.html";
+            driver.Manage().Window.Maximize();
+            var loginBtn = driver.FindElement(By.Id("Login"));
+            Assert.IsTrue(loginBtn.Displayed);
+            driver.Quit();
+        }
+        [Test]
+        public void FirstSeleniumTest()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + @"\Drivers");
+            driver.Url = "file:///c:/Workspace/homepage.html";
+            var elementsList = driver.FindElements(By.XPath("//a"));
+
+            foreach (var elm in elementsList)
+            {
+                Assert.IsTrue(elm.Displayed);
+            }
+            driver.Quit();
+        }
+    }
+}
